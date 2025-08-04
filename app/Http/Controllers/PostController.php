c9f5e2331e04a3883d93fa34bed819add7838fa8
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-08-04 23:38:25 +0600

    config env. file and send email to admin after a post created using smtp mailtrap

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Mail;
 
 class PostController extends Controller
 {
-    //
+    public function Create(Request $request) {
+        $post = Post::create([
+            "title"=> $request->title,
+            "content"=> $request->content,
+        ]);
+
+        //notify admin
+        Mail::raw("A new post '{$post->title}' has been published", function($message){
+            $message->to("admin@email.com")->subject("New post published");
+        });
+
+
+        //notify users
+
+        return back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('create');
 });
+
+Route::post('create/post', [PostController::class,'Create']);
